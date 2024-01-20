da3998585ed963387d6046f5305e65112e4e0c7c
LucasPires50 <luc_50@outlook.com>
2024-01-20 09:39:03 -0300

    feat: configure and add hendouts

---
+++ apostilas/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.messages import constants
+from .models import Apostila, ViewApostila
+
+def adicionar_apostilas(request):
+    if request.method == 'GET':
+        apostilas = Apostila.objects.filter(user=request.user)
+        views_totais = ViewApostila.objects.filter(apostila__user = request.user).count()
+        return render(request, 'adicionar_apostilas.html', {'apostilas': apostilas, 'views_totais': views_totais})
+    elif request.method == 'POST':
+        titulo = request.POST.get('titulo')
+        arquivo = request.FILES['arquivo']
+
+        apostila = Apostila(user=request.user, titulo=titulo, arquivo=arquivo)
+        apostila.save()
+        messages.add_message(
+            request, constants.SUCCESS, 'Apostila adicionada com sucesso.'
+        )
+        return redirect('/apostilas/adicionar_apostilas/')
